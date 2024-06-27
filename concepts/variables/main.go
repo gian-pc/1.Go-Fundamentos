0c583c2718704c7f9d2c3a95efcc21199629f91d
gian-pc <gpaucarcortez@gmail.com>
2024-06-27 18:07:53 -0500

    declarar variables 1

---
+++ concepts/variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// primera forma
+	var apple string
+	var banana string
+	var orange string
+
+	apple = "Manzana"
+	banana = "Banana"
+	orange = "Naranja"
+
+	fmt.Println(apple, banana, orange)
+
+}
