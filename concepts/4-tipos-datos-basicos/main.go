ca7fafe8d773f1009577520817fcd3f1fb4429c2
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 15:37:54 -0500

    tipo de dato - numeric - 1

--- concepts/4-tipos-datos-basicos/main.go
+++ concepts/4-tipos-datos-basicos/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	// Tipo de dato: string
+	// Tipo de dato: numeric - uint8: 0 - 255
 
-	var a string = "gian-pc"
+	var edad uint8 = 38
 
-	fmt.Printf("Tipo: %T, Valor: %v\n", a, a)
+	fmt.Printf("Tipo: %T, Valor: %v\n", edad, edad)
 }
