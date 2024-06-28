1a4de5ae3f4cbd3ff36d129fbec7a1d78a63ad08
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 18:43:23 -0500

    operadores de asignación

--- concepts/5-operadores-aritmeticos/main.go
+++ concepts/5-operadores-aritmeticos/main.go
@@ -6,6 +6,11 @@ func main(){
 	// operadores aritmeticos () * / % + - en orden de jerarquía
 
 	var a = (2 + 3)* 2
-	fmt.Println(a)
+	fmt.Println(a) //10
+
+	// operadores de asignación
+	var b int = 5
+	b += 2
+	fmt.Println(b)//7
 
 }
