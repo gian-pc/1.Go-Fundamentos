3d3abf733e0a6808d8ae83e85e69186ce8df5ede
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 16:07:13 -0500

    tipo de dato - numeric - 5 - casting

--- concepts/4-tipos-datos-basicos/main.go
+++ concepts/4-tipos-datos-basicos/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	// Tipo de dato: float32
+	// No se pueden hacer operaciones con diferentes tipos de datos: uint8 - uint16
 
-	var a float32 = 123.24
+	var a uint8 = 200
+	var b uint16 = 2500
 
-	fmt.Printf("Tipo: %T, Valor: %v\n", a, a) // float32, 123.24
+	// solución para poder realizar la operación suma: 
+	// casting 'a', pero nunca va a cambiar el tipo de dato de 'a' uint8
+	c := uint16(a) + b
+
+	fmt.Printf("Tipo: %T, Valor: %v\n", c, c) // uint16, 2700
+	fmt.Printf("Tipo: %T, Valor: %v\n", a, a) // uint8, 200
 }
