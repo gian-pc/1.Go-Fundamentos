88cf4bfb36b26a3efe123ce84f688e0b177e5738
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 15:40:16 -0500

    tipo de dato - numeric - 2

--- concepts/4-tipos-datos-basicos/main.go
+++ concepts/4-tipos-datos-basicos/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	// Tipo de dato: numeric - uint8: 0 - 255
+	// Tipo de dato: numeric - byte alias uint8
 
-	var edad uint8 = 38
+	var edad byte = 38
 
 	fmt.Printf("Tipo: %T, Valor: %v\n", edad, edad)
 }
