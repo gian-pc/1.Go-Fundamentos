4bea5e43bddc8ff62346d98591d1c096d8a15559
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 18:55:24 -0500

    incremento ++, decremento --

--- concepts/5-operadores-aritmeticos/main.go
+++ concepts/5-operadores-aritmeticos/main.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 func main(){
-	// operadores aritmeticos () * / % + - en orden de jerarquía
+	// operadores aritmeticos: () * / % + - en orden de jerarquía
 
 	var a = (2 + 3)* 2
 	fmt.Println(a) //10
 
-	// operadores de asignación
+	// operadores de asignación: = += -= *= /= %=
 	var b int = 5
 	b += 2
 	fmt.Println(b)//7
+
+	// Expresión vs Declaración
+	// Expresión: una expresión es una parte de código que me produce un valor (línea 8)
+	// Declaración: Es solo una instrucción del lenguaje que realiza  una acción
+	// esta declaración puede incrementar (++) o decrementar (--) la variable en 1
+
+	var c int = 7
+	c++
+	fmt.Println(c) //8
 
 }
