e08f68ce93396fe3d2a3f05df91b62290ad41b40
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 15:54:08 -0500

    tipo de dato - numeric - 4

--- concepts/4-tipos-datos-basicos/main.go
+++ concepts/4-tipos-datos-basicos/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	// Tipo de dato: numeric - rune -unicode
+	// Tipo de dato: float32
 
-	var a rune = 'a'
+	var a float32 = 123.24
 
-	fmt.Printf("Tipo: %T, Valor: %v\n", a, a) // int32, 97
+	fmt.Printf("Tipo: %T, Valor: %v\n", a, a) // float32, 123.24
 }
