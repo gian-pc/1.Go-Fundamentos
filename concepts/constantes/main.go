96222b3d69836bd11c7f348d58acd0865d7f6f22
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 11:34:04 -0500

    constantes 5

--- concepts/constantes/main.go
+++ concepts/constantes/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
+// es recomendable que las constantes esten a nivel de package
+const os, domain= "Linux", "ed.team"
+
 func main(){
-	/* TODO: Si se puede inferir el tipo si eliminamos el tipo de dato
-		en este caso infiere "string" e "int"
-	*/
-	const os, domain= "Linux", 1
+	
 	fmt.Println(os, domain)
 }
