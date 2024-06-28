b3a03c16b4618af0a44da2de1c04c16e8bf2eb39
gian-pc <gpaucarcortez@gmail.com>
2024-06-28 16:10:48 -0500

    tipo de dato - numeric - 6 - _blank

--- concepts/4-tipos-datos-basicos/main.go
+++ concepts/4-tipos-datos-basicos/main.go
@@ -8,10 +8,8 @@ func main(){
 	var a uint8 = 200
 	var b uint16 = 2500
 
-	// solución para poder realizar la operación suma: 
-	// casting 'a', pero nunca va a cambiar el tipo de dato de 'a' uint8
-	c := uint16(a) + b
+	// operador _blank: sirve para poder nombrar una lógica y no nos de problema al ejecutarla
+	_ = uint16(a) + b
 
-	fmt.Printf("Tipo: %T, Valor: %v\n", c, c) // uint16, 2700
 	fmt.Printf("Tipo: %T, Valor: %v\n", a, a) // uint8, 200
 }
