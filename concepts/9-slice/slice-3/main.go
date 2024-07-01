3386082346263a0239a7e1580e076022edbded1c
gian-pc <gpaucarcortez@gmail.com>
2024-07-01 15:32:21 -0500

    slice-3 - valor cero

--- concepts/9-slice/slice-3/main.go
+++ concepts/9-slice/slice-3/main.go
@@ -6,11 +6,15 @@ func main(){
 	
 	
 	//pets := []string{"perro", "gato"}
-	pets := make([]string,0,3)
-	pets = append(pets, "conejo", "tortuga", "loro")
+	//pets := make([]string,0,3)
+	//pets = append(pets, "conejo", "tortuga", "loro")
 
-	fmt.Println("pets: " , pets) 
-	fmt.Println("tamaño pets: ", len(pets)) 
-	fmt.Println("capacidad pets: ", cap(pets)) 
+	var pets []string
+
+
+	fmt.Println("pets: " , pets) //[]
+	fmt.Println("tamaño pets: ", len(pets)) //0
+	fmt.Println("capacidad pets: ", cap(pets)) //0
+	fmt.Println("valor cero: ", pets == nil) //true
 
 }
