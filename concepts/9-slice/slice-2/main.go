42fca6f2717f1f5068b63470dd8da2806b57c1a7
gian-pc <gpaucarcortez@gmail.com>
2024-07-01 15:14:35 -0500

    slice - 2 - Go genera  un nuevo aray

--- concepts/9-slice/slice-2/main.go
+++ concepts/9-slice/slice-2/main.go
@@ -6,11 +6,17 @@ func main(){
 	animals := [5]string{"gorila", "perro", "gato", "ave", "elefante"}
 	pets := animals[1:3] // perro, gato
 
-	// agregamos un nuevo elemento tortuga y llegamos a la capacidad máxima
-	pets = append(pets, "conejo", "tortuga")
+	// agregamos un nuevo elemento loro y pasa algo extraño nuevamente
+	// el arreglo inicial animal ya no subre ninguna modificación
+	// la capacidad se vuelve 8
+	pets = append(pets, "conejo", "tortuga", "loro")
 
-	fmt.Println("animals: " , animals) // animals:  [gorila perro gato conejo tortuga]
-	fmt.Println("pets: " , pets) // pets:  [perro gato conejo tortuga]
-	fmt.Println("tamaño pets: ", len(pets)) // 4
-	fmt.Println("capacidad pets: ", cap(pets)) // 4
+	// Array[4]{"perro", "gato", "ave", "elefante"}
+	// cuando alcanzamos la capacidad maxima ya no podemos seguir modificando el array y Go crea un nuevo array de 8
+	// new Array[8]{"perro", "gato", "conejo", "tortuga", "loro"} // 5
+
+	fmt.Println("animals: " , animals) // animals:  [gorila perro gato ave elefante]
+	fmt.Println("pets: " , pets) // pets:  [perro gato conejo tortuga loro]
+	fmt.Println("tamaño pets: ", len(pets)) // 5
+	fmt.Println("capacidad pets: ", cap(pets)) // 8
 }
