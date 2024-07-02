b8cd8bd4ece8d5e437effe2162349de624d01d96
gian-pc <gpaucarcortez@gmail.com>
2024-07-01 20:30:31 -0500

    estructuras-2 punteros

--- concepts/11-estructuras/main.go
+++ concepts/11-estructuras/main.go
@@ -22,13 +22,29 @@ func main(){
 	}
 
 	// segunda forma
-	jose := Person{"Jose", 33, true}
+	//jose := Person{"Jose", 33, true}
 
 	// tercera forma
-	andres := Person{Age:20} //si solo quiero agregar el dato Age
+	//andres := Person{Age:20} //si solo quiero agregar el dato Age
+
+	//podemos crear punteros a estructuras
+	alvaro := &gian // alvaro es un puntero de gian
+
+
+	//desde el puntero podemos hacer modificaciones
+	// * operador de desreferenciación
+	(*alvaro).Age = 50
+	//alvaro.Age = 60 Go también nos da otra forma de hacer modificaciones
+
+
+	//fmt.Printf("%+v\n", gian)
+	//fmt.Printf("%+v\n", gian.Name) //si solo quiero acceder a Name
+	//fmt.Printf("%+v\n", jose)
+	//fmt.Printf("%+v\n", andres)
 
 	fmt.Printf("%+v\n", gian)
-	fmt.Printf("%+v\n", gian.Name) //si solo quiero acceder a Name
-	fmt.Printf("%+v\n", jose)
-	fmt.Printf("%+v\n", andres)
+	fmt.Printf("%+v\n", alvaro)
+
+
+	
 }
