f6ffc8e8cdb5e057a74e394faab97c5f20c78804
gian-pc <gpaucarcortez@gmail.com>
2024-07-02 23:13:03 -0500

    switch - operadores lógicos y condicionales

--- concepts/12-control-flujo/switch/main.go
+++ concepts/12-control-flujo/switch/main.go
@@ -4,11 +4,14 @@ import "fmt"
 
 func main(){
 	character := "🧟"
+	canSearch := false
 
-	switch character{
-	case "🦸", "🧞":
+	switch {
+	case !canSearch:
+		fmt.Println("no esta permitida la busqueda")
+	case character == "🦸"|| character == "🧞":
 		fmt.Println("es un superheroe")
-	case "🦹", "🧟":
+	case character == "🦹" || character == "🧟":
 		fmt.Println("es un supervillano")
 	default:
 		fmt.Println("es un personaje normal")
