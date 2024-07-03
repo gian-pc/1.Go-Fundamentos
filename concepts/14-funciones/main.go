098434b8822299cae2a69d5bce51983e1bebb93f
gian-pc <gpaucarcortez@gmail.com>
2024-07-03 16:02:42 -0500

    funciones con parámetros por referencia

--- concepts/14-funciones/main.go
+++ concepts/14-funciones/main.go
@@ -5,14 +5,15 @@ import (
 	"strings"
 )
 
-// Las funciones por defecto van a trabajar con parámetros por valor
+// Si queremos modificar un valor debemos trabajar con funciones que pasan parámetros por referencia
+// para ello debemos usar punteros
 func main(){
 	name := "gian"
-	toUpperCase(name)
+	toUpperCase(&name) // de puntero a string se hace con el operador de dirección &
 
-	fmt.Println(name) // gian : no se llega a modificar a mayúscula el nombre gian
+	fmt.Println(name) // GIAN : Si se modificó
 }
 
-func toUpperCase(text string){
-	text = strings.ToUpper(text)
+func toUpperCase(text *string){ // ya no queremos recibir un tipo de dato string sino puntero de esta manera obtengo la referencia de la variable name y lo que obtengo es una dirección de memoria
+	*text = strings.ToUpper(*text) // debo trabajar con el operador de referenciación * para acceder al valor
 }
