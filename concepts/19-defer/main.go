c4b8d013e1147e42cb6e541ee0b2d189e27720b8
gian-pc <gpaucarcortez@gmail.com>
2024-07-04 17:57:24 -0500

    defer - 5

--- concepts/19-defer/main.go
+++ concepts/19-defer/main.go
@@ -1,17 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	num := 4
-	// la función diferida hace que Go antes de agregar a la pila evalua los argumentos de mi función en este caso num = 4
-	defer fmt.Println("imprime", num) // 1. se agrega esta función a la pila     5. se ejecuta la funcion diferida
+	// en la práctica esto para que me funciona
+	// esto es bastante util para limpiar recursos
+	// cerrar archivos
+	// cerrar conexiones de red
+	// cerrar controladores de base de datos
 
-	num = 10 // 2. actualizamos el número 10
-	fmt.Println(num) // 3. se imprime el número 10
-	
-	// 4. la función main retorna
+	file, err :=  os.Create("test.txt") // nos va permitir generar un archivo en nuestro sistema operativo
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	_, err = file.Write([]byte("Hello gophers")) // una vez creado el archivo voy a escribir contenido dentro del archivo test.txt
+	if err != nil {
+		file.Close() // si se presenta un error y no podamos escribir se tendría que cerrar el archivo y así limpiar recursos
+		fmt.Println(err)
+		return
+	}
+
+	file.Close() // una vez escrito en el archivo test.txt lo voy a cerrar
 }
 
-// output: 10, imprime 4
+
+// ejecutamos go run main.go y comprobamos si se a creado el archivo test.txt
+// leemos el archivo creado: cat test.txt
