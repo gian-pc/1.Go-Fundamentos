354d281379961285431e02701345a797385c1d97
gian-pc <gpaucarcortez@gmail.com>
2024-07-04 17:23:46 -0500

    defer - 2

--- concepts/19-defer/main.go
+++ concepts/19-defer/main.go
@@ -8,10 +8,11 @@ func main(){
 	// y hasta que momento queremos aplazar esa ejecución
 	// hasta que la función en donde fue llamada el defer retorne
 
+	defer fmt.Println(3) // defer: me va aplazar la ejecución del número 3
 	fmt.Println(1)
 	fmt.Println(2)
-	fmt.Println(3)
+	
 
 }
 
-// output: 1 2 3
+// output: 1 2 vuelve a el return de la función main y ejecuta los valores diferidos(3)
