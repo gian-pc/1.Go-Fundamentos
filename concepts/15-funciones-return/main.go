558e0aaf8b7722ba22e034581a98e8e0169a9077
gian-pc <gpaucarcortez@gmail.com>
2024-07-03 19:02:56 -0500

    funciones con retornos nombrados

--- concepts/15-funciones-return/main.go
+++ concepts/15-funciones-return/main.go
@@ -10,10 +10,10 @@ func main(){
 	fmt.Println(lower, upper)
 }
 
-// funciones con multiples retornos
-func convert(text string) (string, string){
-	lower := strings.ToLower(text)
-	upper := strings.ToUpper(text)
+// funciones con multiples retornos - nombrados
+func convert(text string) (lower string, upper string){
+	lower = strings.ToLower(text)
+	upper = strings.ToUpper(text)
 
-	return lower, upper
+	return
 }
