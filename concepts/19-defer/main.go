29e4efad384b9525fe00465d51b96ea675e384cb
gian-pc <gpaucarcortez@gmail.com>
2024-07-04 17:27:29 -0500

    defer - 3

--- concepts/19-defer/main.go
+++ concepts/19-defer/main.go
@@ -2,17 +2,24 @@ package main
 
 import "fmt"
 
+// pila
+defer fmt.Println(1)
+defer fmt.Println(2)
+defer fmt.Println(3)
+
 func main(){
 	// Defer: La función defer nos permite diferir algo
 	// diferir significa aplazar, que queremos aplazar la ejecución de una función
 	// y hasta que momento queremos aplazar esa ejecución
 	// hasta que la función en donde fue llamada el defer retorne
 
-	defer fmt.Println(3) // defer: me va aplazar la ejecución del número 3
-	fmt.Println(1)
-	fmt.Println(2)
+	// cuando se tiene multiples funciones diferidas se va agregar a la pila
+	// una vez la función main retorne va ejecutar la funciones en la pila 1, 2 , 3
+	defer fmt.Println(3)
+	defer fmt.Println(2)
+	defer fmt.Println(1)
 	
 
 }
 
-// output: 1 2 vuelve a el return de la función main y ejecuta los valores diferidos(3)
+// output: 1 2 3
