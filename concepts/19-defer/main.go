0f74e290f4b5414ef6c680b641963d998fc63be9
gian-pc <gpaucarcortez@gmail.com>
2024-07-04 18:02:13 -0500

    defer - 6

--- concepts/19-defer/main.go
+++ concepts/19-defer/main.go
@@ -18,16 +18,15 @@ func main(){
 		return
 	}
 
+	// de esta  manera se puede implementar defer en nuestro codigo
+	defer file.Close() // con esto me aseguro que siempre se cierre el archivo
+
 	_, err = file.Write([]byte("Hello gophers")) // una vez creado el archivo voy a escribir contenido dentro del archivo test.txt
 	if err != nil {
-		file.Close() // si se presenta un error y no podamos escribir se tendría que cerrar el archivo y así limpiar recursos
 		fmt.Println(err)
 		return
 	}
-
-	file.Close() // una vez escrito en el archivo test.txt lo voy a cerrar
 }
-
 
 // ejecutamos go run main.go y comprobamos si se a creado el archivo test.txt
 // leemos el archivo creado: cat test.txt
