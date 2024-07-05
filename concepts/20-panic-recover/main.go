e57667a3f25a2d346476cdcd55aa8ef57d636b1f
gian-pc <gpaucarcortez@gmail.com>
2024-07-04 22:43:01 -0500

    recover

--- concepts/20-panic-recover/main.go
+++ concepts/20-panic-recover/main.go
@@ -6,12 +6,17 @@ func main(){
 
 	division(100,10) // 10
 	division(200,25) // 8
-	division(34, 0) // panic
-	division(124, 8) // no se ejecuta porque el programa entro en pánico y se detiene la ejecución
+	division(34, 0) // me recuperé del panic
+	division(124, 8) // 15
 
 }
 
 func division(dividend, divisor int){
+	defer func ()  {
+		if r := recover(); r != nil{
+			fmt.Println("me recupere del panic")
+		}
+	}()
 	validateZero(divisor)
 	fmt.Println(dividend / divisor)
 }
