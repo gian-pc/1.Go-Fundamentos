2b2d968c5068abe262684cb3c3d209ad2e8b5bdf
gian-pc <gpaucarcortez@gmail.com>
2024-07-05 17:43:30 -0500

    Args - Arg

--- edls/main.go
+++ edls/main.go
@@ -9,24 +9,24 @@ func main(){
 	// utilizando el paquete flag: nos va permitir hacer esos programas de lineas de comando
 	// es un package de la librería standar
 
-	// filter pattern
-	flagPattern := flag.String("p", "", "filter by pattern")
-	flagAll := flag.Bool("a", false, "all files including hide files")
-	flagNumberRecords := flag.Int("n", 0, "number of records") // flagNumberRecords
+	// filter pattern --> flags que va a filtrar la entrada
+	// flagPattern := flag.String("p", "", "filter by pattern")
+	// flagAll := flag.Bool("a", false, "all files including hide files")
+	// flagNumberRecords := flag.Int("n", 0, "number of records") // flagNumberRecords
 
-	// order flags
-	hasOrderByTime := flag.Bool("t", false, "sort by time, oldest first")
+	// order flags --> flags que va a ordenar la salida
+	/* hasOrderByTime := flag.Bool("t", false, "sort by time, oldest first")
 	hasOrderBySize := flag.Bool("s", false, "sort by file size, smallest first")
-	hasOrderReverse := flag.Bool("r", false, "reverese order while sorting")
+	hasOrderReverse := flag.Bool("r", false, "reverese order while sorting") */
 	
 	flag.Parse() // Parse va a mapear cada uno de los flags y los va a almacenar en flagPattern
+	path := flag.Arg(1) // con Arg ontengo un solo argumento en función del índice
+	fmt.Println(path)
+	fmt.Println(flag.Args()) // con Args obtengo todos los argumentos
+	// fmt.Println("pattern:",*flagPattern)
+	// fmt.Println("all:",*flagAll)
+	// fmt.Println("flagNumberRecords:",*flagNumberRecords)
 	
-	fmt.Println("pattern:",*flagPattern)
-	fmt.Println("all:",*flagAll)
-	fmt.Println("flagNumberRecords:",*flagNumberRecords)
-	fmt.Println("hasOrderByTime:",*hasOrderByTime)
-	fmt.Println("hasOrderBySize:",*hasOrderBySize)
-	fmt.Println("hasOrderReverse:",*hasOrderReverse)
 }
 
 // para ejecutar ubicarse dentro de edls
@@ -45,3 +45,8 @@ func main(){
 // flag -s: $ go run . -p .png -a -n 10 -t -s
 
 // flag -r: $ go run . -p .png -a -n 10 -t -s -r
+
+
+// para ejecutar: go run . /home/gian/Desktop/testFiles edteam godesdecero
+// output: 	edteam
+// 			[/home/gian/Desktop/testFiles edteam godesdecero]
