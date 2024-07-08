bcbdc4e120534feca908845316a3d0b2231f740a
gian-pc <gpaucarcortez@gmail.com>
2024-07-08 17:21:46 -0500

    Generics - solución genericos any

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	PrintListString("a", "b", "c")
-	// problema: que  pasa si quiero imprimir números
-	PrintListInt(1, 2, 3)
+	PrintList("a", "b", "c")
+	PrintList(1, 2, 3)
 }
 
-func PrintListString(list ...string){
-	for _, item := range list{
-		fmt.Println(item)
-	}
-}
-
-// posible solución: se repite codigo
-func PrintListInt(list ...int){
+// solución:  genericos - any
+func PrintList(list ...any){
 	for _, item := range list{
 		fmt.Println(item)
 	}
