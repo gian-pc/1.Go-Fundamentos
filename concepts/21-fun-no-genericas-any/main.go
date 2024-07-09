67bbf2558e2f1bacc20a96f44a9263199058aaa8
gian-pc <gpaucarcortez@gmail.com>
2024-07-09 14:39:16 -0500

    constrains a  partir de interface

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -9,13 +9,16 @@ func main(){
 	var num2 MyInt = 6
 
 	fmt.Println(sum(2,4,67))
-	fmt.Println(sum(2.2,4.6,67.1))
+	fmt.Println(sum[float32](2.0,4.6,67.1))
 	fmt.Println(sum(num1, num2))
 }
 
-// por ende si se quiere que la función suma sea de forma generica soporte todos esos tipos de datos
-// una posible solución sería incrementar esos tipos de datos en los parámetros
-func sum[T ~int | ~float64 | ~float32 | ~uint](nums ...T) T { // en este caso solo estamos restringiendo int y float64 sin embargo hay mas tipos de datos numericos
+// Otra solución: constrains a partir de interface
+type Number interface{
+	~int | ~float64 | ~float32 | ~uint
+}
+
+func sum[T Number](nums ...T) T { 
 	var total T
 	for _, num := range nums{
 		total += num
