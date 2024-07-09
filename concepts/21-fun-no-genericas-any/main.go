155bdba68b1f42cb360caa69615e4f4e5668ab3b
gian-pc <gpaucarcortez@gmail.com>
2024-07-08 23:26:17 -0500

    Generics - la solución no es any

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -8,9 +8,14 @@ func main(){
 	fmt.Println(sum(2.4, 5.5, 7.2)) // no se podría y se generaría un error ya que nuestra función esta limitada a int
 }
 
-// solución:  genericos - any
-func sum(nums ...int)int{
-	var total int
+// posible solución:  genericos - any --> se generaría un error debido al alcance de any
+// ya que estamos haciendo una operación invalida y que el operador + no esta definida sobre la variable total
+// ya que total es de tipo any
+// al decirlo que es de tipo any, le estas diciendo que puede recibir cualquier tipo de dato(bool, string, int)
+// y eso no es lo que queremos, lo que queremos es sumar enteros y flotantes
+// por lo tanto any no sería la solución
+func sum(nums ...any) any {
+	var total any
 	for _, num := range nums{
 		total += num
 	}
