7cadeecf7c02c69b076d525a4ccea25ab14183c3
gian-pc <gpaucarcortez@gmail.com>
2024-07-09 00:08:32 -0500

    Generic - trabajando con nuestro propio tipo de dato

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
+type MyInt int
+
 func main(){
+	var num1 MyInt = 2
+	var num2 MyInt = 6
+
 	fmt.Println(sum[int](2,4,67))
 	fmt.Println(sum[float64](2.2,4.6,67.1))
+	fmt.Println(sum[MyInt](num1, num2))
 }
 
-// - constrains de unión de elementos
-func sum[T int | float64](nums ...T) T {
+// - tipos de datos definidos --> enteros, flotantes, strings, booleanos, etc.
+// - se pueden también crear nuestros propios tipos de datos basados en eso datos existentes
+func sum[T int | float64 | MyInt](nums ...T) T {
 	var total T
 	for _, num := range nums{
 		total += num
