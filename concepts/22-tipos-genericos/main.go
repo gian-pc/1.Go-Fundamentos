7f97f745cfd9d433c3b774477edb84febbc30635
gian-pc <gpaucarcortez@gmail.com>
2024-07-09 17:40:17 -0500

    tipos genericos - solución genericos

--- concepts/22-tipos-genericos/main.go
+++ concepts/22-tipos-genericos/main.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
-type Product struct{
-	ID	 		 uint
+type Product[T uint | string] struct{
+	ID	 		 T
 	Description	 string
 	Price	 	 float64
-}
-
-type ProductV2 struct{
-	ID 			 string
-	Description  string
-	Price 		 float64
 }
 
 func main(){
@@ -20,12 +14,10 @@ func main(){
 	// caso de ejemplo: si tenemos una DB en postgres con id entero y luego queremos pasarlo
 	// a mongoDB con un id string "6f9b13ce-d9e8" se tendría que generar otra estructura
 
-	product1 := Product{ID: 1, Description: "Zapatos", Price: 30}
-	product2 := ProductV2{ID: "6f9b13ce-d9e8", Description: "Zapatos", Price: 30}
-
+	product1 := Product[uint]{ID: 1, Description: "Zapatos", Price: 30}
+	product2 := Product[string]{ID: "6f9b13ce-d9e8", Description: "Zapatos", Price: 30}
 
 	fmt.Println(product1)
 	fmt.Println(product2)
-
 
 }
