d180ec8fa4860f59979c33393e7d45f938b0b7e9
gian-pc <gpaucarcortez@gmail.com>
2024-07-08 23:51:12 -0500

    Generics - constraint de unión de elemento

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -4,19 +4,11 @@ import "fmt"
 
 func main(){
 	fmt.Println(sum[int](2,4,67))
-
+	fmt.Println(sum[float64](2.2,4.6,67.1))
 }
 
-// entoces es aquí donde entran los parámetros de tipo
-// estos parámetros de tipo nos van a permitir definir restricciones para trabajar con multiples tipos de datos ya sean funciones o en tipos como estructura
-// entonces cual sería la restricción en la función suma, pues recibir solo datos de tipo enteros y flotantes
-// los parámetros de tipo se van a definir en corchetes
-// existen 3 tipos de restricciones o constrains
-	// - constrains arbitrario
-	// - constrains de unión de elementos
-	// - constrains de aproximación de elementos
-
-func sum[T int](nums ...T) T {
+// - constrains de unión de elementos
+func sum[T int | float64](nums ...T) T {
 	var total T
 	for _, num := range nums{
 		total += num
