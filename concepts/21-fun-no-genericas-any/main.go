9874bdc6e350f793ab8c3213c14e2af0c6347704
gian-pc <gpaucarcortez@gmail.com>
2024-07-09 00:15:54 -0500

    Generic - Go infiere el tipo de dato segun el primer elemento

--- concepts/21-fun-no-genericas-any/main.go
+++ concepts/21-fun-no-genericas-any/main.go
@@ -8,9 +8,9 @@ func main(){
 	var num1 MyInt = 2
 	var num2 MyInt = 6
 
-	fmt.Println(sum[int](2,4,67))
-	fmt.Println(sum[float64](2.2,4.6,67.1))
-	fmt.Println(sum[MyInt](num1, num2))
+	fmt.Println(sum(2,4,67))
+	fmt.Println(sum(2.2,4.6,67.1))
+	fmt.Println(sum(num1, num2))
 }
 
 // - tipos de datos definidos --> enteros, flotantes, strings, booleanos, etc.
